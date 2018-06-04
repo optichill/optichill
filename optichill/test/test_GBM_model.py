ecf97503eededceb15deb800c9af59a77a5d6575
Caitlin Parke <parkec3@uw.edu>
2018-06-04 16:48:46 -0700

    Created test subdirectory for unit tests for optichill

---
+++ optichill/test/test_GBM_model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.ensemble import GradientBoostingRegressor
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+import seaborn as sns
+import glob
+import os
+from dataCleaning import bas_filter
