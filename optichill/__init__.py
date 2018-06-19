5e11e4137d43448cdea4fa717cf1a9635c98eecf
Ted Cohen <tacohen@uw.edu>
2018-06-19 11:06:53 -0700

    retooled init

--- optichill/__init__.py
+++ optichill/__init__.py
@@ -1,4 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = ["GBM_model","bas_filter"]
+
 from .version import __version__
-from .optichill import *
+from optichill import bas_filter
+from optichill import GBM_model
 
 # __all__ = [__version__, index_return]
