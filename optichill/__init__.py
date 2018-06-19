e857d86c2f5886c6f0d8056a2535ea0a99a4e4ba
Ted Cohen <tacohen@uw.edu>
2018-06-19 11:13:10 -0700

    commented out gbm

--- optichill/__init__.py
+++ optichill/__init__.py
@@ -4,6 +4,6 @@ __all__ = ["GBM_model","bas_filter"]
 
 from .version import __version__
 from optichill import bas_filter
-from optichill import GBM_model
+#from optichill import GBM_model
 
 # __all__ = [__version__, index_return]
